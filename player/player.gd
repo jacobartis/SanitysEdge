c2c0b383f0a4611e9a140f66d60e5f30083d6cab
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-06-18 15:07:31 +0100

    Removed hand attacks, added jumping and air velocity.

--- player/player.gd
+++ player/player.gd
@@ -15,24 +15,28 @@ func _process(delta):
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if Input.is_action_just_pressed("l_attack"):
-		get_tree().call_group("l_weapon_display","shoot")
-	if Input.is_action_just_pressed("r_attack"):
-		get_tree().call_group("r_weapon_display","shoot")
+	if Input.is_action_just_pressed("attack"):
+		$Camera3D/Hand.attack()
 	if Input.is_action_just_pressed("reload"):
-		get_tree().call_group("l_weapon_display","reload")
+		get_tree().call_group("weapon_display","reload")
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-	else:
+	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED/100)
+		velocity.z = move_toward(velocity.z, 0, SPEED/100)
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	elif Input.is_action_just_pressed("jump"):
+		velocity.y += JUMP_VELOCITY
 	move_and_slide()
 
 func _input(event):
@@ -43,3 +47,6 @@ func move_camera(motion:InputEventMouseMotion):
 	var movement = motion.get_relative()*Settings.mouse_sense
 	$Camera3D.rotate_x(-deg_to_rad(movement.y))
 	rotate_y(-deg_to_rad(movement.x))
+
+func take_damage(val:float):
+	print("ow ",val)
